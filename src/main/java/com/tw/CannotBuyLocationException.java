1a25fba8944e2c471c1c8c41f927316d0648de81
xushanchuan <scxu@thoughtworks.com>
2015-10-20 15:26:08 +0800

    remove unused class

--- src/main/java/com/tw/CannotBuyLocationException.java
+++
@@ -1,9 +0,0 @@
-package com.tw;
-
-import com.tw.exception.RichGameException;
-
-public class CannotBuyLocationException extends RichGameException {
-    public CannotBuyLocationException(String message) {
-        super(message);
-    }
-}
